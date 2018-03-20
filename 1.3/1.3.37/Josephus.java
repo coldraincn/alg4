0e0ca14584ab69babaabda58f15f0d864ba26975
coldraincn <handan213@gmail.com>
2018-03-20 21:59:58 +0800

    init

---
+++ 1.3/1.3.37/Josephus.java
@@ -0,0 +1,12 @@
+
+public class Josephus {
+    public static void main(String[] args) {
+        int m = Integer.parseInt(args[0]);
+        int n = Integer.parseInt(args[1]);
+
+    
+       
+            StdOut.print(m + " ");
+      
+    }
+}
