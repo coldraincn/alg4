351c51514944bf8a65cd8e329bd9263baf2b268f
coldraincn <handan213@gmail.com>
2018-03-21 21:23:06 +0800

    1.3.37

--- 1.3/1.3.37/Josephus.java
+++ 1.3/1.3.37/Josephus.java
@@ -3,10 +3,21 @@ public class Josephus {
     public static void main(String[] args) {
         int m = Integer.parseInt(args[0]);
         int n = Integer.parseInt(args[1]);
+        Queue<Integer> queue=new Queue<Integer>();
+        for(int i =0;i<n;i++){
+            queue.enqueue(i);
+        }
+        while(!queue.isEmpty()){
+            for(int j=1;j<m;j++ ){
+                queue.enqueue(queue.dequeue());
+            }
+            StdOut.print(queue.dequeue());
+        }
+
 
     
        
-            StdOut.print(m + " ");
+        StdOut.println();
       
     }
 }
